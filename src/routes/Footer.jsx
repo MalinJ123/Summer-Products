4f3b906934fa9eebe1ad4a14362428beb25a1801
MalinJ123 <malin--jonsson@hotmail.com>
2023-05-03 13:58:23 +0200

    footer looks fine tillsvidare

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -4,16 +4,17 @@ const Footer = () => {
 			<div className="footer-body">
 				
 				<div>
-					<p>Öppettider</p>
-					<p>Mån-Fre</p>
-					<p>07:00 - 15:00</p>
+					<p className="openhour">Öppettider</p>
+					<p className="days">Mån-Fre</p>
+					<p className="days">07:00 - 15:00</p>
 				</div>
 
 				<div className="icons"></div>
 
 				<div className="location">
-					<p>Drottninggatan 13</p>
-					<p>070 ~ 23 23 234 </p>
+					<p className="openhour">Drottninggatan 13</p>
+					<p>Karlstad</p>
+
 				</div>
 			</div>
 		</>
