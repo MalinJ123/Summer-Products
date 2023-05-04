e555638eea0c769bf0ce9692d84931aac3ef6f0b
MalinJ123 <malin--jonsson@hotmail.com>
2023-05-04 15:12:08 +0200

    lagt till en sök funktion i produkts

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,40 +1,56 @@
-import React from "react";
-import { useActionData } from "react-router-dom";
+import React, { useState } from "react";
 import { useLoaderData, Link } from "react-router-dom";
 import "../stylesheet/Products.css";
 import Suunto from "../assets/Suunto.jpg";
 import Footer from "../components/Footer";
-import {getProducts} from '../data/getProducts.js'
+import { getProducts } from "../data/getProducts.js";
 
-
-export const loader = () => getProducts()
+export const loader = () => getProducts();
 
 const Products = () => {
-	const productData = useLoaderData();
-	return (
-		<section className="products-body">
-			<h2 className="productTitle">Products</h2>
-			<div className="grid-container">
-				{productData.map(({ id, name, picture, price }) => (
-					<div className="product" key={id}>
-						<h3 className="products-title">
-							<Link to={`/details/${id}`}>{name}</Link>
-						</h3>
-						<img
-							className="products-pic"
-							src={picture}
-							alt="bild på produkt"
-						/>
-						{/* <p className="empty">{description}</p> */}
-						<div className="product-price">
-							<p>Pris:{price}kr</p>
-						</div>
-					</div>
-				))}
-			</div>
-		
-		</section>
-	);
+  const productData = useLoaderData();
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const filteredProducts = productData.filter((product) =>
+    product.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  return (
+    <section className="products-body">
+      <h2 className="productTitle">Products</h2>
+
+      <input
+        onChange={handleChange}
+        className="search-input"
+        type="text"
+        placeholder="Sök på produkt"
+      />
+     
+      <div className="grid-container">
+        {filteredProducts.map(({ id, name, picture, price }) => (
+          <div className="product" key={id}>
+            <h3 className="products-title">
+              <Link to={`/details/${id}`}>{name}</Link>
+            </h3>
+            <img
+              className="products-pic"
+              src={picture}
+              alt="bild på produkt"
+            />
+            <div className="product-price">
+              <p>Pris:{price}kr</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* <Footer /> */}
+    </section>
+  );
 };
 
 export default Products;
