252668de6af67c268960e8aaec25e750eb2838a1
MalinJ123 <malin--jonsson@hotmail.com>
2023-05-04 22:38:04 +0200

    börjar se ut som ett admin formulär, css ej helt klar, validering är inte ens påtänkt

--- src/routes/AdminLogin.jsx
+++ src/routes/AdminLogin.jsx
@@ -1,20 +1,43 @@
 import paradise from "../assets/paradise.jpg";
+import { useLoaderData, useParams, Link, useNavigate } from "react-router-dom";
+import "../stylesheet/AdminLogin.css";
+// import '../stylesheet/Home.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-
-const AdminLogin =() =>{
-	 return(
+const AdminLogin = () => {
+	return (
 		<>
-		<img
-			className="home-pic"
-			src={paradise}
-			alt="En härlig paradis bild"
-		/>
-		<div className="content">
-			<h2 className="title"> ADMIN </h2>
-		
-		</div>
-
-	</>
-	 )
-}
+			<img
+				className="admin-pic"
+				src={paradise}
+				alt="En härlig paradis bild"
+			/>
+			<form className="form-container" action="">
+				<div className="close-button">
+					{" "}
+					<Link className="" to="/">
+						<FontAwesomeIcon icon={faTimes} />
+					</Link>
+				</div>
+				<h2 className="AdminLogin-title">Admin login</h2>
+				<div className="label-container">
+					<label className="input-text" htmlFor="">
+						<p className="text">Username</p>
+						<input className="input" type="text" />
+					</label>
+					<label htmlFor="">
+						<p className="text">Password</p>
+						<input className="input" type="text" />
+					</label>
+				</div>
+				<button className="admin-button">
+					<Link className="link-btn" to="/">
+						Tillbaka
+					</Link>
+				</button>
+			</form>
+		</>
+	);
+};
 export default AdminLogin;
